2187a7671440bf9a8247c533f51975a13648a3d7
redbelter <jupiterrun@proton.me>
2024-05-31 23:01:08 -0700

    fsdfs
    cfsdfs

---
+++ redmods.ps1
@@ -0,0 +1,8 @@
+$steamPath = Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Valve\Steam -Name InstallPath
+$steamPath = $steamPath.InstallPath
+if(-not (Test-Path $steamPath -PathType Leaf)){
+
+}
+
+$myJson = Get-Content .\currentlist.txt -Raw | ConvertFrom-Json 
+$myJson
